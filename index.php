b1f739e653c707ccae9971688d652facb5929f20
Yomyom25 <131711625+Yomyom25@users.noreply.github.com>
2025-03-06 10:56:19 -0600

    autentificacion correcta

--- index.php
+++ index.php
@@ -3,12 +3,10 @@ if (isset($_SESSION["autentificado"]) && $_SESSION["autentificado"] === "SI") {
     header("Location: principal.php");
     exit();
 }
-
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,35 +14,43 @@ if (isset($_SESSION["autentificado"]) && $_SESSION["autentificado"] === "SI") {
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <title>Login</title>
 </head>
+
 <body>
+    <div class="div-1200px">
+        <div class="img-logo">
+            <a href="index.php">
+                <img class="logo" src="img/hospital-icon.png" alt="Logo">
+            </a>
+        </div>
+        <div class="background_login">
+            <h1>Iniciar sesión</h1>
+            <form action="autentificar.php" method="post" name="login_plantilla">
+                <?php
+                // $errorusuario = isset ($_GET["errorusuario"]);
+                // if($errorusuario == "SI") {
+                //     echo "<p class='error'>Usuario o contraseña incorrectos</p>";
+                // }
+                if (isset($_GET["error"])) {
+                    if ($_GET["error"] == "campos_vacios") {
+                        echo "<p class='error'>Debes llenar ambos campos.</p>";
+                    } elseif ($_GET["error"] == "usuario_incorrecto") {
+                        echo "<p class='error'>El usuario no existe.</p>";
+                    } elseif ($_GET["error"] == "contrasena_incorrecta") {
+                        echo "<p class='error'>La contraseña es incorrecta.</p>";
+                    }
+                }
+                ?>
+                <input type="text" name="usuario" placeholder="Nombre de usuario" class="input-login ancho-uniforme"
+                    value="<?php echo isset($_GET['usuario']) ? htmlspecialchars($_GET['usuario']) : ''; ?>">
+                <input type="password" name="contrasena" placeholder="Contraseña" class="input-login ancho-uniforme ">
+                <br>
+                <input type="submit" value="Iniciar sesión" class="btn-login ancho-uniforme btn">
+            </form>
 
-<div class="div-1200px">
-
-<div class="img-logo">
-    <a href="index.php">
-        <img class="logo" src="img/hospital-icon.png" alt="Logo">
-    </a>
+        </div>
     </div>
-
-    <div class="background_login">
-        <h1>Iniciar sesión</h1>
-        <form action="autentificar.php" method="post" name="login_plantilla">
-            <?php
-            $errorusuario = isset ($_GET["errorusuario"]);
-            if($errorusuario == "SI") {
-                echo "<p class='error'>Usuario o contraseña incorrectos</p>";
-            }
-            ?>
-            <input type="text" name="usuario" placeholder="Nombre de usuario" class="input-login ancho-uniforme ">
-            <input type="password" name="contrasena" placeholder="Contraseña" class="input-login ancho-uniforme ">
-             <br>
-            <input type="submit" value="Iniciar sesión" class="btn-login ancho-uniforme btn">
-        </form>
-
     </div>
-    </div>
-
-</div>
-    <script src="scripts/validacion.js"></script> 
+    <script src="scripts/validacion.js"></script>
 </body>
+
 </html>
